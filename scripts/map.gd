4198304296622c83545cb4b78917494dedb92d1d
bommbomm34 <umitalmanya8@gmail.com>
2025-07-26 09:49:08 +0200

    Limited camera in Map
    Also removed GitPlugin as VCS to avoid autoload errors on startup

--- scripts/map.gd
+++ scripts/map.gd
@@ -2,4 +2,7 @@ extends Control
 
 func _input(event: InputEvent) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) or event is InputEventScreenDrag:
-		$Camera2D.position += -Input.get_last_mouse_velocity() * get_process_delta_time()
+		var addition = -Input.get_last_mouse_velocity() * get_process_delta_time()
+		var result = $Camera2D.position + addition
+		if not (result.x < 480 or result.x > 1439 or result.y < 271 or result.y > 809):
+			$Camera2D.position = result
