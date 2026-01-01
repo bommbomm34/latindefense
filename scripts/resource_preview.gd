8c940d11f762d8c4df32950fc495dcad760e6af2
bommbomm34 <bommbomm34@perhof.org>
2026-01-01 17:41:43 +0100

    Add damage label to soldiers

--- scripts/resource_preview.gd
+++ scripts/resource_preview.gd
@@ -4,11 +4,21 @@ var rarity: int
 var resource_name: String
 var price: int
 var amount: int
+var damage: int
 
 func init(given_resource_name: String, resource_image: Texture2D, common_price: int, rare_price: int, legendary_price: int):
 	resource_name = given_resource_name
 	rarity = Rarity.LEGENDARY if resource_name.contains("legendary") else Rarity.RARE if resource_name.contains("rare") else Rarity.COMMON
 	price = legendary_price if rarity == Rarity.LEGENDARY else rare_price if rarity == Rarity.RARE else common_price
+	if resource_name.contains("soldier"):
+		match rarity:
+			Rarity.COMMON:
+				damage = 20
+			Rarity.RARE:
+				damage = 35
+			Rarity.LEGENDARY:
+				damage = 50
+		$DamageLabel.text = "+" + str(damage)
 	$Preview.texture = resource_image
 	$BuyButton.add_theme_color_override("font_color", Color.YELLOW if rarity == Rarity.LEGENDARY else Color.GREEN if rarity == Rarity.RARE else Color.WHITE)
 	$BuyButton.text = tr("Buy for ") + str(price)
