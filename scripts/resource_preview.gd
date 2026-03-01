25965272d78bc6c903c528c438405def830946ac
bommbomm34 <bommbomm34@perhof.org>
2026-03-01 07:42:30 +0100

    Add multiplier labels for farmers

--- scripts/resource_preview.gd
+++ scripts/resource_preview.gd
@@ -4,21 +4,14 @@ var rarity: int
 var resource_name: String
 var price: int
 var amount: int
-var damage: int
+var damage: String
 
 func init(given_resource_name: String, resource_image: Texture2D, common_price: int, rare_price: int, legendary_price: int):
 	resource_name = given_resource_name
 	rarity = Rarity.LEGENDARY if resource_name.contains("legendary") else Rarity.RARE if resource_name.contains("rare") else Rarity.COMMON
 	price = legendary_price if rarity == Rarity.LEGENDARY else rare_price if rarity == Rarity.RARE else common_price
-	if resource_name.contains("soldier"):
-		match rarity:
-			Rarity.COMMON:
-				damage = 20
-			Rarity.RARE:
-				damage = 35
-			Rarity.LEGENDARY:
-				damage = 50
-		$DamageLabel.text = "+" + str(damage)
+	set_damage()
+	$DamageLabel.text = "+" + damage
 	$Preview.texture = resource_image
 	$BuyButton.add_theme_color_override("font_color", Color.YELLOW if rarity == Rarity.LEGENDARY else Color.GREEN if rarity == Rarity.RARE else Color.WHITE)
 	$BuyButton.text = tr("Buy for ") + str(price)
@@ -35,3 +28,21 @@ func _on_buy_button_pressed() -> void:
 		Database.set_value(resource_name + "_amount", amount + 1)
 		Database.set_value("denar", Database.get_value("denar", 0) - price)
 		get_parent().get_parent().get_parent().reload()
+
+func set_damage():
+	if resource_name.contains("soldier"):
+		match rarity:
+			Rarity.COMMON:
+				damage = "20"
+			Rarity.RARE:
+				damage = "35"
+			Rarity.LEGENDARY:
+				damage = "50"
+	elif resource_name.contains("farmer"):
+		match rarity:
+			Rarity.COMMON: 
+				damage = "0.1x"
+			Rarity.RARE:
+				damage = "0.3x"
+			Rarity.LEGENDARY:
+				damage = "0.5x"
